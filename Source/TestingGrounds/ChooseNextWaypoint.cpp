852d0a50bd3794d9c81ef3a24858aab305cc423e
Mehmet Cengiz <m.cengiz4716@gmail.com>
2017-07-28 16:48:05 +0300

    PatrollingGuard decoupled from ThirdPersonCharacter as PatrolRoute component.

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -3,19 +3,26 @@
 #include "TestingGrounds.h"
 #include "ChooseNextWaypoint.h"
 #include "AIController.h"
-#include "PatrollingGuard.h"
+#include "PatrolRoute.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory){
 
+
 	//TODO protect against empty patrol route.
 
 	//Get the patrol points
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP; //TODO Implement Getter.
+	auto ControlledPawn = OwnerComp.GetAIOwner() -> GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+
+	if(PatrolPoints.Num() == 0 ){
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing patrol points"));
+		return EBTNodeResult::Failed;
+	}
 	
 	//Set next waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
