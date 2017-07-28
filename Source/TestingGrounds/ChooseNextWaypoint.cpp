9334670fe6b4e8b925c21d729b4b645dadca27db
Mehmet Cengiz <m.cengiz4716@gmail.com>
2017-07-28 14:57:13 +0300

    PatrollingGuards Patrols between patroling points with ChooseNextWaypoint.CPP

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -2,13 +2,29 @@
 
 #include "TestingGrounds.h"
 #include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "PatrollingGuard.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory){
+
+	//TODO protect against empty patrol route.
+
+	//Get the patrol points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP; //TODO Implement Getter.
 	
+	//Set next waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	//Cycle the index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
 	//UE_LOG(LogTemp, Warning, TEXT("AI in C++ - yay!"));
 	return EBTNodeResult::Succeeded;
 }
--- Source/TestingGrounds/PatrollingGuard.h
+++ Source/TestingGrounds/PatrollingGuard.h
@@ -13,8 +13,8 @@ class TESTINGGROUNDS_API APatrollingGuard : public ATP_ThirdPersonCharacter
 {
 	GENERATED_BODY()
 
+public:	//TODO create Getter methods.
 	UPROPERTY(EditInstanceOnly,Category="Patrol Route")	
 	TArray<AActor*> PatrolPointsCPP;
-	
 	
 };
