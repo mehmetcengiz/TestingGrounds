a0992f8d5ba8df1386f5451de65115f920dc679c
Mehmet Cengiz <m.cengiz4716@gmail.com>
2017-08-07 22:13:31 +0300

    TestingGroundsGameMode default pawn referance uncomented and referanced to Character .

--- Source/TestingGrounds/TestingGroundsGameMode.cpp
+++ Source/TestingGrounds/TestingGroundsGameMode.cpp
@@ -9,8 +9,8 @@ ATestingGroundsGameMode::ATestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Blueprint/FirstPersonCharacter"));
-	//DefaultPawnClass = PlayerPawnClassFinder.Class;
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Character/Blueprint/Character"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
 	HUDClass = ATestingGroundsHUD::StaticClass();
