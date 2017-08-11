27f14076c361224f48069e695ed0ba0c6878e86f
Mehmet Cengiz <m.cengiz4716@gmail.com>
2017-08-11 14:56:03 +0300

    Probs are has random scale and random rotation.

--- Source/TestingGrounds/Terrain/Tile.cpp
+++ Source/TestingGrounds/Terrain/Tile.cpp
@@ -27,15 +27,17 @@ void ATile::Tick(float DeltaTime)
 
 }
 
-void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn,float Radius){
+void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn,float Radius, float MinScale, float MaxScale){
 
 	int NumberToSpawn = FMath::RandRange(MinSpawn,MaxSpawn);
 
 	for (int i = 0; i < NumberToSpawn; ++i){
 		FVector SpawnPoint;
-		bool found = FindEmptyLocation(SpawnPoint, Radius);
+		float RandomScale = FMath::RandRange(MinScale, MaxScale);
+		bool found = FindEmptyLocation(SpawnPoint, Radius * RandomScale);
 		if(found){
-			PlaceActor(ToSpawn, SpawnPoint);
+			float RandomRotation = FMath::RandRange(-180.f, 180.f);
+			PlaceActor(ToSpawn, SpawnPoint,RandomRotation,RandomScale);
 		}
 	}
 }
@@ -57,10 +59,12 @@ bool ATile::FindEmptyLocation(FVector& OutLocation,float Radius){
 	return false;
 }
 
-void ATile::PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint){
+void ATile::PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint, float Rotation, float Scale){
 	AActor* Spawned = GetWorld()->SpawnActor<AActor>(ToSpawn);
 	Spawned->SetActorRelativeLocation(SpawnPoint);
 	Spawned->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative,false));
+	Spawned->SetActorRotation(FRotator(0, Rotation, 0));
+	Spawned->SetActorScale3D(FVector(Scale));
 }
 
 bool ATile::CanSpawnAtLocation(FVector Location, float Radius){
@@ -79,7 +83,7 @@ bool ATile::CanSpawnAtLocation(FVector Location, float Radius){
 	);
 
 	FColor ResultColor = HasHit ? FColor::Red : FColor::Green;
-	DrawDebugCapsule(GetWorld(), GlobalLocation, 0, Radius, FQuat::Identity, ResultColor, true,100);
+	//DrawDebugCapsule(GetWorld(), GlobalLocation, 0, Radius, FQuat::Identity, ResultColor, true,100); // For debug.
 
 	return !HasHit;
 }
